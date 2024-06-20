926799ed8f1a114693d2c6c5c4da2f6b0a99dcab
Bhuvnesh-Rana <bhuvnesh.rana28@gmail.com>
2024-06-20 10:20:42 +0530

    Initial Contreoller test

---
+++ src/test/java/com/mongo/Controller/MongoControllerTest.java
@@ -0,0 +1,35 @@
+package com.mongo.Controller;
+
+import org.junit.jupiter.api.Test;
+
+public class MongoControllerTest {
+    @Test
+    void testAddEmp() {
+
+    }
+
+    @Test
+    void testDeleteEmployee() {
+
+    }
+
+    @Test
+    void testGetAllEmployees() {
+
+    }
+
+    @Test
+    void testGetEmployeeById() {
+
+    }
+
+    @Test
+    void testShow() {
+
+    }
+
+    @Test
+    void testUpdateEmployee() {
+
+    }
+}
